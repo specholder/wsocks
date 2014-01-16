b96e85fc5671abb8b96ed0f1da7d3584d58be114
Araki Shigeyuki <ara.ki@bb.wakwak.com>
2014-01-16 22:36:24 +0900

    設定ファイル情報のWEBサーバーへの適用

--- config/config.js
+++ config/config.js
@@ -1,8 +1,11 @@
 module.exports = {
 	http: {
+		server: true,
+		daemon: 'socket', // 'port' or 'socket'
 		port: 3000,
-		uid: "www-data",
-		access_log: ""
+		socket: __dirname + '/../logs/wsocks-httpd.sock',
+		uid: 'www-data',
+		access_log: ''
 	},
 	socketio: {}
 };
