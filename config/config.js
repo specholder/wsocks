119ef1bbffe5e21c92d24a688fab5619209ad727
Araki Shigeyuki <ara.ki@bb.wakwak.com>
2014-01-19 23:31:22 +0900

    [http] 静的ファイルの表示（途中）

--- config/config.js
+++ config/config.js
@@ -1,10 +1,22 @@
 module.exports = {
 	http: {
 		server: true,
-		daemon: 'socket', // 'port' or 'socket'
+		daemon: 'port', // 'port' or 'socket'
 		port: 3000,
 		socket: __dirname + '/../logs/wsocks-httpd.sock',
 		uid: 'www-data',
+		dirindex: ['index.html', 'index.htm'],
+		mime: {
+			html: 'text/html',
+			htm : 'text/html',
+			css : 'text/css',
+			js  : 'application/javascript',
+			jpg : 'image/jpeg',
+			jpeg: 'image/jpeg',
+			png : 'image/png',
+			gif : 'image/gif',
+			ico : 'image/x-icon'
+		},
 		access_log: ''
 	},
 	socketio: {}
